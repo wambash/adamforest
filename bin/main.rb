ef4e8f9e5949a82fca7df3af1f00a2bb37d3d073
chazzka <adamo.ulrich@gmail.com>
2023-05-24 15:24:41 +0200

    Revert "Forest constructors"

--- bin/main.rb
+++ bin/main.rb
@@ -7,11 +7,9 @@ include AdamForest
 
 puts "main"
 
-batch_size = 128
-forest = Forest.new([[1, 1], [2, 2], [3, 3],[1, 1], [2, 2], [3, 3],[1, 1], [2, 2], [3, 3],[1, 1], [2, 2], [3, 3], [10000, 1000]], trees_count: 4, batch_size: batch_size)
+s = Node.init_from_data([[2, 2], [3, 3], [7, 1000]], ForestHelperServiceDimensional)
 
-# evaluation = forest.evaluate_forest([2.5, 1.5])
-# If instances return s very close to 1, then they are definitely anomalies,
-depths = forest.evaluate_forest_return_depths([10000, 10000])
-s = Helper.evaluate_anomaly_score_s(depths, batch_size)
-p s
+p s.to_j
+
+#fs = ForestHelperServiceDimensional.forest_count_split_point([[2, 2], [3, 3], [7, 8]])
+#p ForestHelperServiceDimensional.node_group_by([[2, 2], [3, 3], [7, 8]], fs)
