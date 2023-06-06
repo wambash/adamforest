269d4e67fb97ccc0e70f0f35fe1b326511217919
wambash <wamba@centrum.cz>
2023-06-06 14:45:14 +0200

    removed some prints

--- test/services/test_service_quicksort.rb
+++ test/services/test_service_quicksort.rb
@@ -10,9 +10,7 @@ class TestServiceQuicksort < Minitest::Test
   def test_quick_sort
     input = [5, 8, 3, 4, 2, 7]
     forest = Forest.new(input, trees_count: 1, forest_helper: QuickSort, random: Random.new(3))
-    p forest.trees.first.to_h
-    p forest.trees.first.to_a
-    p res = forest.evaluate_forest(6).first
+    res = forest.evaluate_forest(6).first
     assert_equal res, [7]
   end
 end
